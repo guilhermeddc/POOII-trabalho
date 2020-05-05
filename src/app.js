d3615818c29120ded4fc8e3f013765e74c5f2727
Guilherme <guilhermeddc@gmail.com>
2020-05-05 12:14:13 -0300

    ultimos ajustes

--- src/app.js
+++ src/app.js
@@ -27,18 +27,6 @@ class App {
   routes () {
     this.server.use(routes)
   }
-
-  exceptionHandler () {
-    this.server.use(async (err, req, res, next) => {
-      if (process.env.NODE_ENV === 'development') {
-        const errors = await new Youch(err, req).toJSON()
-
-        return res.status(500).json(errors)
-      }
-
-      return res.status(500).json({ error: 'Internal server error' })
-    })
-  }
 }
 
 export default new App().server
